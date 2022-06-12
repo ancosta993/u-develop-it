9e99e1547dd9ce3c7e44e10ce38d9577f0f825ac
Anthony Dcosta <adcosta2000@gmail.com>
2022-06-12 13:43:30 -0400

    referenced parties table to the candidates table

--- server.js
+++ server.js
@@ -21,8 +21,11 @@ const db = mysql.createConnection(
 
 // Get all candidates
 app.get('/api/candidates', (req, res) => {
+   const sql = `SELECT candidates.*, parties.name
+               AS party_name FROM candidates
+               LEFT JOIN parties ON candidates.party_id = parties.id`;
 
-   db.query(`SELECT * FROM candidates`, (err, rows)=>{
+   db.query(sql, (err, rows)=>{
       if (err) {
          res.status(500).json({error: err.message});
          return;
@@ -38,7 +41,10 @@ app.get('/api/candidates', (req, res) => {
 
 // GET a single candidate
 app.get('/api/candidate/:id', (req, res) => {
-   const sql = `SELECT * FROM candidates WHERE id = ?`
+   const sql = `SELECT candidates.*, parties.name
+   FROM candidates
+   LEFT JOIN parties ON candidates.party_id = parties.id
+   WHERE candidate.id = ?`
    const params = [req.params.id];
 
    db.query(sql, params, (err, row)=>{
